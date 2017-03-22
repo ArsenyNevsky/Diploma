bd01231777ddb5931f6c5a6a0527b94f3f6ddfc7
Arseny Nevsky <erafiil@gmail.com>
2017-03-23 01:30:36 +0300

    Deleted extra  comments

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,17 @@
 package main;
 
+import main.quant.Quant;
+import main.spline.SplineWaveletCompress;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
 
     private static int[] a = {1, 2, 3, 4, 5, 0, 0, 0, 100, 200, 300, -40, -60, -80, -100, 0, 0, 0, 0};
 
     public static void main(String[] args) {
-        /*System.out.println("Length = " + a.length + "\nArray = " + Arrays.toString(a));
+        System.out.println("Length = " + a.length + "\nArray = " + Arrays.toString(a));
         SplineWaveletCompress swc = new SplineWaveletCompress(a);
         swc.compress();
         Quant quantMainStream = new Quant();
@@ -19,8 +25,7 @@ public class Main {
         System.out.println("After quant B: " + waveStream);
         ArrayList<Double> streamWave = quantWaveStream.getConverseQuantStream(waveStream);
 
-        swc.decompress(net, streambase, streamWave);*/
-        int[] a = {10, 20, 30, 40, 50};
+        swc.decompress(net, streambase, streamWave);
 
     }
 }
